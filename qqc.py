8c86f60422a966add36ae18e8c335b267d129db8
Pedro_Luca <pedroluca2000@gmail.com>
2026-02-12 16:58:28 +0000

    changed entropy

--- qqc.py
+++ qqc.py
@@ -7,6 +7,6 @@ def get_rho(Pressure, Temperature):
     return rho 
 
 enthalpy = PropsSI("H", "P", 50e5, "Q", 0, FLUID)
-
+entropy = PropsSI("S", "P", 1e5, "Q", 0, FLUID)
 rho_exp = get_rho(50e5, 280)
-print(f"rho={rho_exp} kg/m3 \nenthalpy={enthalpy/1e3} kJ/kg")
+print(f"rho={rho_exp} kg/m3 \nenthalpy={enthalpy/1e3} kJ/kg \nentropy={entropy/1e3} kJ/K")
